383172b165698709aa1a8d7f3ae03939a769433c
justxale <alexev2401@gmail.com>
2026-05-26 12:48:05 +0300

    feat: use new extractors in campaign routes

--- crates/aurorite-server/src/routes/campaigns.rs
+++ crates/aurorite-server/src/routes/campaigns.rs
@@ -1,5 +1,5 @@
 use crate::database::Campaign;
-use crate::extractors::Authorization;
+use crate::extractors::{AuthorizedAdmin, AuthorizedMaster};
 use crate::requests::PostCampaign;
 use crate::responses::{AuroriteErrorResponse, CampaignInfo, FailableResponse};
 use crate::state::AuroriteState;
@@ -10,32 +10,43 @@ use axum::routing::{get, post};
 use axum::{Json, Router};
 use uuid::Uuid;
 
+#[axum::debug_handler]
 async fn get_campaign(
     Path(id): Path<Uuid>,
     State(state): State<AuroriteState>,
-    user: Authorization,
+    admin_client: Option<AuthorizedAdmin>,
+    master_client: Option<AuthorizedMaster<true>>
 ) -> FailableResponse<CampaignInfo> {
     let mut db = state.db();
-    match Campaign::filter_by_id(id)
-        .include(Campaign::fields().clients())
-        .get(&mut db).await
-    {
-        Err(err) => Err((StatusCode::NOT_FOUND, AuroriteErrorResponse::new(err).json())),
-        Ok(ref res) => match CampaignInfo::try_from(res) {
-            Ok(res) => Ok((StatusCode::OK, res.json())),
-            Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, AuroriteErrorResponse::new("failed to build response").json()))
-        }
+    let record = match (admin_client, master_client) {
+        (Some(AuthorizedAdmin(_)), _) => {
+            match Campaign::filter_by_id(id)
+                .include(Campaign::fields().clients())
+                .include(Campaign::fields().races())
+                .include(Campaign::fields().classes())
+                .get(&mut db).await
+            {
+                Err(err) => Err((StatusCode::NOT_FOUND, AuroriteErrorResponse::new(err).json())),
+                Ok(res) => Ok(res)
+            }
+        },
+        (None, Some(AuthorizedMaster(_, campaign))) => Ok(campaign),
+        _ => Err((StatusCode::UNAUTHORIZED, AuroriteErrorResponse::new("neither an admin nor a master").json()))
+    }?;
+    match CampaignInfo::try_from(&record) {
+        Ok(res) => Ok((StatusCode::OK, res.json())),
+        Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, AuroriteErrorResponse::new("failed to build response").json()))
     }
 }
 
 async fn post_campaign(
     State(state): State<AuroriteState>,
-    user: Authorization,
+    AuthorizedAdmin(client): AuthorizedAdmin,
     Json(body): Json<PostCampaign>,
 ) -> FailableResponse<CampaignInfo> {
     let record = Campaign::create()
         .title(body.title)
-        .owner_id(user.id());
+        .owner_id(client.id);
     let mut db = state.db();
     match record.exec(&mut db).await {
         Err(err) => Err((StatusCode::NOT_FOUND, AuroriteErrorResponse::new(err).json())),
@@ -49,5 +60,5 @@ async fn post_campaign(
 pub fn build_campaign_routes() -> Router<AuroriteState> {
     Router::new()
         .route("/", post(post_campaign))
-        .route("/{id}", get(get_campaign))
+        .route("/{campaign_id}", get(get_campaign))
 }
