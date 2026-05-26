bc6471d8a2db35adc9f702a62731c1044c1d87ed
justxale <alexev2401@gmail.com>
2026-05-26 12:30:38 +0300

    feat: implement OptionalFromRequestParts for jwt extractors

--- crates/aurorite-server/src/extractors.rs
+++ crates/aurorite-server/src/extractors.rs
@@ -1,21 +1,20 @@
 pub use crate::utils::jwt::Authorization;
 use crate::utils::jwt::{KEYS, TokenError};
-use axum::RequestPartsExt;
-use axum::extract::{FromRequestParts, Path};
+use axum::extract::{FromRequestParts, OptionalFromRequestParts, Path};
 use axum::http::request::Parts;
+use axum::RequestPartsExt;
 use axum_extra::headers;
 use axum_extra::headers::authorization::Bearer;
 use axum_extra::typed_header::TypedHeader;
 use jsonwebtoken::Validation;
 use jsonwebtoken::decode;
-use serde::{Deserialize, de::Error};
 use crate::database::{Campaign, CampaignClient, Client};
 use crate::state::AuroriteState;
 use crate::utils::uuid::EncodedUuid;
 
 pub struct AuthorizedClient(pub Client);
 pub struct AuthorizedAdmin(pub Client);
-pub struct AuthorizedMaster(pub Client, pub Campaign);
+pub struct AuthorizedMaster<const LOAD_CAMPAIGN: bool>(pub Client, pub Campaign);
 
 
 impl FromRequestParts<AuroriteState> for AuthorizedClient {
@@ -57,23 +56,62 @@ impl FromRequestParts<AuroriteState> for AuthorizedAdmin {
     }
 }
 
-impl FromRequestParts<AuroriteState> for AuthorizedMaster {
+impl OptionalFromRequestParts<AuroriteState> for AuthorizedAdmin {
+    type Rejection = TokenError;
+
+    async fn from_request_parts(parts: &mut Parts, state: &AuroriteState) -> Result<Option<Self>, Self::Rejection> {
+        match <AuthorizedAdmin as FromRequestParts<AuroriteState>>::from_request_parts(parts, state).await {
+            Ok(res) => Ok(Some(res)),
+            Err(_) => Ok(None)
+        }
+    }
+}
+
+impl<const LOAD_CAMPAIGN: bool> FromRequestParts<AuroriteState> for AuthorizedMaster<LOAD_CAMPAIGN> {
     type Rejection = TokenError;
     async fn from_request_parts(parts: &mut Parts, state: &AuroriteState) -> Result<Self, Self::Rejection> {
         let AuthorizedClient(client) = AuthorizedClient::from_request_parts(parts, state).await?;
-        let Path(EncodedUuid(campaign_id)) = Path::<EncodedUuid>::from_request_parts(parts, state)
+        let Path(EncodedUuid(campaign_id)) = <Path<EncodedUuid> as FromRequestParts<AuroriteState>>::from_request_parts(parts, state)
             .await
             .map_err(|err| TokenError::NotFound(err.to_string()))?;
-        if client.campaigns.is_unloaded() {
-            return Err(TokenError::Failed("campaigns not loaded".into()));
+        let mut db = state.db();
+        match LOAD_CAMPAIGN {
+            true => {
+                let record = CampaignClient::filter_by_client_id_and_campaign_id(client.id, campaign_id)
+                    .include(CampaignClient::fields().campaign())
+                    .include(CampaignClient::fields().campaign().classes())
+                    .include(CampaignClient::fields().campaign().races())
+                    .include(CampaignClient::fields().campaign().clients())
+                    .get(&mut db)
+                    .await
+                    .map_err(|_| TokenError::NotFound(format!("campaign {} not found", campaign_id)))?;
+                if client.is_admin || record.is_master {
+                    Ok(Self(client, record.campaign.get().clone()))
+                } else {
+                    Err(TokenError::NotMaster)
+                }
+            },
+            false => {
+                let record = CampaignClient::get_by_client_id_and_campaign_id(&mut db, client.id, campaign_id)
+                    .await
+                    .map_err(|_| TokenError::NotFound(format!("campaign {} not found", campaign_id)))?;
+                if client.is_admin || record.is_master {
+                    Ok(Self(client, record.campaign.get().clone()))
+                } else {
+                    Err(TokenError::NotMaster)
+                }
+            }
         }
-        let record = CampaignClient::get_by_client_id_and_campaign_id(&mut state.db(), client.id, campaign_id)
-            .await
-            .map_err(|_| TokenError::NotFound(format!("campaign {} not found", campaign_id)))?;
-        if client.is_admin || record.is_master {
-            Ok(Self(client, record.campaign.get().clone()))
-        } else {
-            Err(TokenError::NotMaster)
+    }
+}
+
+impl<const LOAD_CAMPAIGN: bool> OptionalFromRequestParts<AuroriteState> for AuthorizedMaster<LOAD_CAMPAIGN> {
+    type Rejection = TokenError;
+
+    async fn from_request_parts(parts: &mut Parts, state: &AuroriteState) -> Result<Option<Self>, Self::Rejection> {
+        match <AuthorizedMaster<LOAD_CAMPAIGN> as FromRequestParts<AuroriteState>>::from_request_parts(parts, state).await {
+            Ok(res) => Ok(Some(res)),
+            Err(_) => Ok(None)
         }
     }
 }
