c81672ec605db741c3932b0cbc80143c02516eb9
justxale <alexev2401@gmail.com>
2026-05-27 11:52:36 +0300

    fix: use indexed composite keys in campaign models

--- crates/aurorite-server/src/database/models/campaign.rs
+++ crates/aurorite-server/src/database/models/campaign.rs
@@ -18,6 +18,8 @@ pub struct Campaign {
     pub races: HasMany<CampaignRace>,
     #[has_many]
     pub classes: HasMany<CampaignClass>,
+    #[has_many]
+    pub characters: HasMany<CampaignCharacter>,
 
     #[belongs_to(key = owner_id, references = id)]
     owner: BelongsTo<Client>,
@@ -25,27 +27,29 @@ pub struct Campaign {
 
 #[derive(Clone, Debug, Model)]
 pub struct CampaignCharacter {
-    #[key]
-    #[auto]
-    pub id: Uuid,
-
     current_hp: u16,
 
     #[index]
+    #[key]
     character_id: Uuid,
+    #[index]
+    #[key]
+    campaign_id: Uuid,
+
+
     #[belongs_to(key = character_id, references = id)]
     base: BelongsTo<Character>,
+    #[belongs_to(key = campaign_id, references = id)]
+    campaign: BelongsTo<Campaign>,
 }
 
 #[derive(Clone, Debug, Model)]
 pub struct CampaignRace {
-    #[key]
-    #[auto]
-    pub id: u64,
-
     #[index]
+    #[key]
     race_id: Uuid,
     #[index]
+    #[key]
     campaign_id: Uuid,
 
     #[belongs_to(key = race_id, references = id)]
@@ -56,13 +60,11 @@ pub struct CampaignRace {
 
 #[derive(Clone, Debug, Model)]
 pub struct CampaignClass {
-    #[key]
-    #[auto]
-    pub id: u64,
-
     #[index]
+    #[key]
     class_id: Uuid,
     #[index]
+    #[key]
     campaign_id: Uuid,
 
     #[belongs_to(key = class_id, references = id)]
@@ -73,13 +75,11 @@ pub struct CampaignClass {
 
 #[derive(Clone, Debug, Model)]
 pub struct CampaignBackground {
-    #[key]
-    #[auto]
-    pub id: u64,
-
     #[index]
+    #[key]
     background_id: Uuid,
     #[index]
+    #[key]
     campaign_id: Uuid,
 
     #[belongs_to(key = background_id, references = id)]
@@ -90,8 +90,10 @@ pub struct CampaignBackground {
 
 #[derive(Clone, Debug, Model)]
 pub struct CampaignClient {
+    #[index]
     #[key]
     client_id: Uuid,
+    #[index]
     #[key]
     campaign_id: Uuid,
 
